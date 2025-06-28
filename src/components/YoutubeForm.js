9fa2242f3ecd5947f9090da48291a36c525b4dad
Pmparekh2000 <pmp.tech2000@gmail.com>
2025-06-28 12:14:54 +0530

    Added formik fields and onChange

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,17 +1,46 @@
+import { useFormik } from "formik";
 import React from "react";
 
 const YoutubeForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "Siddhu",
+      email: "",
+      channel: "",
+    },
+  });
+
+  console.log("Form values", formik.values);
+
   return (
     <div>
       <form>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
 
         <label htmlFor="email">E-Mail</label>
-        <input type="email" id="email" name="email" />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel" />
+        <input
+          type="text"
+          id="channel"
+          name="channel"
+          onChange={formik.handleChange}
+          value={formik.values.channel}
+        />
 
         <button>Submit</button>
       </form>
